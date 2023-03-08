1d8639103c2734c79782c8122342fd2c28a11619
Christopher Jackson <jacksoch1994@gmail.com>
2023-03-08 16:24:46 -0500

    Complete Dto Objects

--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/model/TransactionDto.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/model/TransactionDto.java
@@ -1,4 +1,24 @@
 package com.techelevator.tenmo.model;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class TransactionDto {
+
+    private int id;
+    @Positive
+    private BigDecimal amount;
+    private int senderId;
+    private int receiverId;
+    private boolean isRequest;
+    private String memo;
+    @NotBlank
+    private String status;
+    @NotNull
+    private LocalDateTime transactionTime=LocalDateTime.now();
+
 }
--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/model/WalletDto.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/model/WalletDto.java
@@ -1,4 +1,13 @@
 package com.techelevator.tenmo.model;
 
+import javax.validation.constraints.Positive;
+import java.math.BigDecimal;
+
 public class WalletDto {
+
+    private int id;
+    private int userId;
+    @Positive
+    private BigDecimal balance;
+
 }
